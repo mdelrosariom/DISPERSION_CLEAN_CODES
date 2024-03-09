69ffe0d011fcc9c15440272134f80d37c6a0396c
María del Rosario Miranda <71475725+mdelrosariom@users.noreply.github.com>
2024-03-09 21:05:47 +0100

    Add files via upload

---
+++ 23_11_23 (1).py
@@ -0,0 +1,322 @@
+# -*- coding: utf-8 -*-
+
+import random as rnd
+import tkinter as tk
+import numpy as np
+import math as math
+from simple_landscape import simple_landscape
+from adaptation import adapt 
+
+nrow = 20
+ncol = 20
+size = 1/4
+main = ncol // 2
+shape = 0
+time_of_adaptation =5 #the adaptation will occur if the time step is multiple of this number
+current_time_step = 0
+niche_mainland = [1,2,3,4,5,6,7,8,9,10,11,12]
+sp_1 = ['sp_1']+ rnd.choices(["A", "T", "C", "G"], k= 100) 
+niche_of_sp1 =  rnd.randint(1,10)
+niche_sp_1 =  list(range(niche_of_sp1, niche_of_sp1+3))
+
+sp_2 = ['sp_2']+ rnd.choices(["A", "T", "C", "G"], k= 100) 
+niche_of_sp2 =  rnd.randint(1,10)
+niche_sp_2 =  list(range(niche_of_sp2, niche_of_sp2+3))
+
+species_list = [sp_1, sp_2]
+
+# time steps of the simulation
+max_time_steps = 10  # 
+
+
+
+mainland_island = simple_landscape(nrow, ncol, size, shape)
+
+class Visual:
+    def __init__(self, max_x, max_y):
+        self.zoom = 15
+        self.max_x = max_x
+        self.max_y = max_y
+        self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root, width=max_x * self.zoom, height=max_y * self.zoom)
+        self.canvas.pack()
+        self.canvas.config(background='royalblue')
+        self.squares = np.empty((self.max_x, self.max_y), dtype=object)
+        self.initialize_squares()
+
+    def initialize_squares(self):
+        '''
+        create the land (island and mainland as brown) and the squares that represent each coordinate
+
+        '''
+        for x in range(self.max_x):
+            for y in range(self.max_y):
+                fill_color = 'saddlebrown' if mainland_island[y, x] != 0 else ''
+                self.squares[x, y] = self.canvas.create_rectangle(
+                    x * self.zoom, y * self.zoom, (x + 1) * self.zoom, (y + 1) * self.zoom,
+                    outline='black', fill=fill_color )
+
+class Plant:
+    def __init__(self, x, y, drawing, specie):
+        self.x = x
+        self.y = y
+        self.drawing = drawing
+        self.age = 0
+        self.pos = [x,y]
+        self.disp_cap = rnd.randint(2,19) #for now random, after we can define diferent species based on this
+        self.specie = specie
+        if specie == "sp_1": 
+            self.color = 'forestgreen'
+            self.niche = niche_sp_1
+        if specie == "sp_2": 
+            self.color = 'blue'
+            self.niche = niche_sp_2
+        self.comp_ab = rnd.random()
+        
+
+def create_plant(x, y,initial_color):
+    '''
+    the plant is created on the windowa in a different function
+    '''
+    radius = 0.5
+    return canvas.create_oval(
+        (x - radius) * visual.zoom, (y - radius) * visual.zoom,
+        (x + radius) * visual.zoom, (y + radius) * visual.zoom,
+        outline='black', 
+        fill = initial_color
+        #fill='forestgreen'
+    )
+
+def niche(mainland_island):
+    '''
+  this will create other matrix that contains the values of the niche of all
+  the landscape. the niche is simply a list of numbers from 0 to 10 (for now)
+  the mainland contains all the niches (from 1 to 12). island will contain a subset 
+  of this niche. a list of lists with 2 or 2 subsets of the mainland. a species 
+  will have a "species niche" that is simply a list of 3 numbers (ascending order,
+  e.g [1 2 3]) if at least one of the numbers of the species niche is in the sequence 
+  of the island niche, then the specie can live there, otherwise, it cant  '''    
+
+    en_niche = np.zeros(mainland_island.shape)
+# Transform it into a list so you can replace the elements
+    en_niche = [[[item] for item in row] for row in en_niche] # need to transform it in lists because its 
+    niche_of_island =  rnd.randint(1,9)  #we need to define this now because if now it will produce 
+#one each time (in every coordinate of the island and we want the same )
+    for i in range(len(en_niche)):
+    
+        for j in range(len(en_niche)): 
+            if mainland_island[i][j] == 1:
+                en_niche[i][j] = niche_mainland #complete niches only 4 availables
+            if mainland_island[i][j] == 2:            
+                en_niche[i][j] = list(range(niche_of_island, niche_of_island+4)) # i am thinking of 
+  #larger than the niche i am thinking for species (len = 3)
+    return en_niche
+            
+environmental_niche = niche(mainland_island)   
+
+
+def dispersal(indiv):
+       
+ #I will use the gaussian for dispersal because its simple and commonly used in dispersal 
+ #studies 
+    dispersal_x = np.random.normal(indiv.disp_cap, 1) #standar derivation common 1 
+    dispersal_y = np.random.normal(indiv.disp_cap, 1)    
+    #a chunck so the seed can be dispersed also to left and down
+    direction = rnd.choice([-1, 1])
+    dx = dispersal_x*direction
+    direction = rnd.choice([-1, 1])
+    dy = dispersal_y*direction    
+    x_new = int((indiv.x + dx) % nrow) #%nrow module to wrap up the world 
+    y_new = int((indiv.y + dy) % ncol) #%nrow module to wrap up the world 
+
+    if mainland_island[int(y_new), int(x_new)] != 0:
+        canvas.coords(indiv.drawing, (x_new - 0.5) * visual.zoom, (y_new - 0.5) * visual.zoom,
+                      (x_new + 0.5) * visual.zoom, (y_new + 0.5) * visual.zoom)
+        indiv.x = x_new
+        indiv.y = y_new       
+
+        return ([indiv.x, indiv.y])
+    else: 
+        pass
+
+visual = Visual(ncol, nrow)
+canvas = visual.canvas
+population = []
+
+# Create individuals only on the mainland
+for _ in range(50):
+    for specie in species_list: 
+        x = int(rnd.uniform(0, main)) #discrete mov
+        y = int(rnd.uniform(0, nrow)) #discrete mov
+        drawing = create_plant(x, y, initial_color='red')       
+        specie = species_list[species_list.index(specie)][0]
+        plant = Plant(x, y, drawing, specie)
+        
+        population.append(plant)
+
+def update():
+    global current_time_step
+    global population  # Declare population as a global variable
+    neighbors = population[:]
+    plants_to_remove = []    
+        #offspring 
+    pop_nei = population[:] #we made this list to find neighbors of plants
+
+    for x in range(len(population)): 
+        plant_obj = population[x]
+        if plant_obj.age > 1:  
+            wo_pl_ob = [x for x in pop_nei if x != plant_obj]
+            
+            for plant_com in wo_pl_ob:
+                if plant_com.specie == plant_obj.specie:
+                    
+                    pos1 = plant_obj.pos
+                       
+                    pos2 = plant_com.pos
+                    
+                    vx = pos1[0]
+                    vy = pos1[1]
+                    nx = pos2[0]
+                    ny = pos2[1]
+                    if vx == nx-1 and vy == ny-1 or vx == nx-1 and vy ==ny or vx == nx-1 and vy == ny+1 \
+                    or vx == nx and vy == ny-1 or vx == nx and vy == ny+1 or vx == nx +1 and vy == ny-1 \
+                    or vx == nx+1 and vy == ny   or vx == nx+1 and vy == ny+1 :
+                        #parents can produce more than 1 seed
+                        num_offspring = rnd.randint(1,4) #now the plant could produce more than 1 seed/ offspring, randomly 
+                        for seed in range(num_offspring):
+                        
+                            parent_disp = rnd.choice([plant_obj,plant_com])
+                            position = dispersal(parent_disp) #for now its that parent
+                            if position != None: #this means, the seed did not end on the sea
+                                x_off = position[0]
+                                y_off =position[1]                            
+                            
+                            #position_off = dispersal(parent_pos)
+                            
+                            #if mainland_island[int(y_off), int(x_off)] != 0: #we already have that condition inside dispersal function
+                            
+                                drawing_off = create_plant(x_off, y_off, plant_obj.color)
+                                offspring = Plant(x_off, y_off, drawing_off, plant_obj.specie)
+                                parent_disp_cap =rnd.choice([plant_obj, plant_com]) #for now we chose one at random, after we can think on codominance
+                                offspring.disp_cap = parent_disp_cap.disp_cap
+                                if current_time_step%time_of_adaptation: #only adapt (shift its niche one place) each 100 time steps 
+                                    if mainland_island[offspring.x][offspring.y] >1: #23 11 23 correction so occurs in future islands #== 2: #adaptation occur in islands
+                                        
+                                        offspring.niche = adapt(environmental_niche[offspring.x][offspring.y],plant_com.niche, "natural_selection", "yes")
+                                population.append(offspring)     
+                          
+            pop_nei.remove(plant_obj)  
+            
+        #plants die after 1 years
+    for plant in population:
+        if plant.age >= 3:
+            canvas.delete(plant.drawing)
+            plants_to_remove.append(plant)     
+            
+     
+    #to kill of the islands all the plants that dont have a compatible niche
+    for plant in population: 
+        if mainland_island[plant.x][plant.y] > 1:  # Only consider islands #this is in the case we build multiple islands in the future
+            if not set(environmental_niche[plant.x][plant.y]) & set(plant.niche):
+                canvas.delete(plant.drawing)
+                plants_to_remove.append(plant)
+            
+            
+# This forms a list with all the plants in the same place (lists called same_place)
+    for plant_1 in population: 
+        same_place = []
+        comp_ab_values = []  # Make a list for later
+       # winner = None  # Initialize winner
+
+        same_place.append(plant_1)
+        rest = [x for x in population if x != plant_1]
+    
+        for plant_2 in rest: 
+            if plant_1.pos == plant_2.pos:
+                same_place.append(plant_2)
+            
+                # To here ok, now we need to pick the one in that list who has better competitive ability
+        if len(same_place) > 1: # Step 1: Make a list with all the values of competitive ability of the plants in that position  
+        #####FIRST WE WILL CHECK BEST ADAPTED !!!!! NEWWW
+           
+            #adaptation.append()
+    #BEST ADAPTED FIRST IN ISLANDS BECAUSE ITS SUPPOSE THAT ALL EQUALLY ADAPTED TO MAINLAND. 
+            if mainland_island[same_place[0].x][same_place[0].y] > 1: #to check if individuals are in islands
+                winner = []
+                best_adapted = []
+                for veg in same_place:    
+                    niche_width = len(set(environmental_niche[veg.x][veg.y]) & set(veg.niche))        
+                    veg.niche_width = niche_width
+                
+                max_niche_width = max(same_place, key=lambda x: x.niche_width) #this magical thing let you choose from which attribute you want the maximum
+                #in our case is niche_width
+                for iv in same_place: 
+                    
+                    if iv.niche_width == max_niche_width.niche_width: 
+                        best_adapted.append(iv)
+                if len(best_adapted) ==1: 
+                    winnner = best_adapted[0]
+                    losers = [x for x in same_place if x !=winner]
+                    
+                else: #if there is multiple individuals best adapted. Ww apply the other criteria of best competitive ability 
+                    rnd.shuffle(same_place) #just in case, so we don't give advantages
+                    
+                    while len(winner) < 1: #process continue until there is a winner
+                        for competitor in same_place:  #we try every plant in the space
+                            chances_of_win = rnd.random() #and compare the value with a random number who gets reassigned each time, 
+                            #for me this is more fair
+                            if chances_of_win < competitor.comp_ab: #check chances
+                                winner.append(competitor) # we win a competitor!
+                            losers = [x for x in same_place if x !=winner]
+                    for los in losers: 
+                        plants_to_remove.append(los)       
+                        
+                    
+                    
+                for los in losers: 
+                    plants_to_remove.append(los) 
+                    
+                    
+            
+           
+            else: # selection of best competitor in island
+            
+        
+        
+        
+                winner = [] #where the winner will be 
+                rnd.shuffle(same_place) #just in case, so we don't give advantages
+                
+                while len(winner) < 1: #process continue until there is a winner
+                    for competitor in same_place:  #we try every plant in the space
+                        chances_of_win = rnd.random() #and compare the value with a random number who gets reassigned each time, 
+                        #for me this is more fair
+                        if chances_of_win < competitor.comp_ab: #check chances
+                            winner.append(competitor) # we win a competitor!
+                        losers = [x for x in same_place if x !=winner]
+                for los in losers: 
+                    plants_to_remove.append(los)    
+  
+                    
+    population = [plant for plant in population if plant not in plants_to_remove]
+
+    for plant in plants_to_remove:
+        canvas.delete(plant.drawing)
+
+    for plant in population:
+       
+        plant.age += 1
+     
+    print(len(population))
+    current_time_step += 1
+    if current_time_step < max_time_steps:
+        # Schedule the next update with an interval
+        visual.root.after(200, update)
+    else:
+        # Stop the simulation when we reach the maximum time steps
+        print("Simulation finished.")
+
+update()
+visual.root.mainloop()
+
+
